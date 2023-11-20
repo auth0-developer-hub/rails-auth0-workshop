342568c3f8614f8a36120ec802373b76973acef0
Carla Urrea Stabile <carla.urrea@okta.com>
2023-11-20 15:39:12 +0100

    add user model

---
+++ db/migrate/20231120143632_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :nickname
+      t.string :name
+      t.string :picture
+      t.string :email
+      t.boolean :email_verified
+      t.string :sub
+
+      t.timestamps
+    end
+  end
+end
