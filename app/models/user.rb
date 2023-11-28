b6352446725c8b799c9f1d2ab356edad81578457
Carla Urrea Stabile <carla.urrea@okta.com>
2023-11-28 14:58:35 +0100

    add uniqueness on nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :messages
   
   validates :role, inclusion: { in: %w( admin ) }, allow_nil: true
+  validates :nickname, presence: true, uniqueness: true
 end
